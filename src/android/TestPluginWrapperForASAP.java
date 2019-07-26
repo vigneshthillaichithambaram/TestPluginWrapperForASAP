443c3f450ac3fb1c93a16ece33af16d523c60e22
vignesh-zt132 <vignesh.thillai@zohocorp.com>
2019-07-26 15:08:51 +0530

    version change

--- src/android/TestPluginWrapperForASAP.java
+++ src/android/TestPluginWrapperForASAP.java
@@ -19,10 +19,23 @@ public class TestPluginWrapperForASAP extends CordovaPlugin {
             this.coolMethod(message, callbackContext);
             return true;
         }
+        if (action.equals("coolMethodDup")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
         return false;
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void coolMethodDup(String message, CallbackContext callbackContext) {
         if (message != null && message.length() > 0) {
             callbackContext.success(message);
         } else {
--- www/TestPluginWrapperForASAP.js
+++ www/TestPluginWrapperForASAP.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'TestPluginWrapperForASAP', 'coolMethod', [arg0]);
 };
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'TestPluginWrapperForASAP', 'coolMethodDup', [arg0]);
+};
